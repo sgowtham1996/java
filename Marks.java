cb21ed2802a0ca0d950e3150d28fae37149d2e40
Gowtham S <35688139+sgowtham1996@users.noreply.github.com>
2018-12-11 10:14:47 +0530

    Object reaction
    create a database, using an object (Keyword)

---
+++ Marks.java
@@ -0,0 +1,8 @@
+package Student.java;
+
+public class Marks {
+    public static void main(String[] args){
+        Student Gowtham = new Student("Gowtham",2017,12345,3.2,4);
+        System.out.println(Gowtham);
+    }
+}
---
+++ Student.java
@@ -0,0 +1,17 @@
+package Student.java;
+
+public class Student {
+    String name;
+    int grad;
+    int ID;
+    double GPA;
+    int abs;
+    public Student(String n, int g, int I, double grades, int ab){
+        this.name = n;
+        this.grad = g;
+        this.ID = I;
+        this.GPA = grades;
+        this.abs = ab ;
+            }
+
+}
